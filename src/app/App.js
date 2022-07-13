e3d788de403d215536331de6cfa43bb03aab31d0
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2022-07-13 00:33:25 -0500

    added in fetch for pokemon data

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,27 @@
+import pokemon from 'pokemon';
+import { useEffect, useState } from 'react';
+
 import Search from '../components/Search';
 import './App.css';
 
 function App() {
+
+  const [inputPokemonTeam, setInputPokemonTeam] = useState([]);
+  const [pokemonTeamData, setPokemonTeamData] = useState([]);
+
   return (
-    <div className="App">
-      <Search/>
+    <div>
+      <Search 
+        inputPokemonTeam={inputPokemonTeam}
+        setInputPokemonTeam={setInputPokemonTeam}
+        pokemonTeamData={pokemonTeamData}
+        setPokemonTeamData={setPokemonTeamData}
+      />
+      <div  style={{display: 'flex', justifyContent: 'center'}}>
+        {pokemonTeamData.map((pokemon, i) => {
+          return <img src={pokemon.sprites.front_default} key={i}/>;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,22 +3,48 @@ import { useState,useEffect } from 'react';
 
 const pokemon = require('pokemon');
 
-export default function Search(){
+export default function Search(props){
 
-    const [pokemonTeam, setPokemonTeam] = useState([]);
+    const [fetchPokemonTeamData, setFetchPokemonTeamData] = useState(false);
+
+     var handleInputChange = (v) => {
+        if(v.length <= 6){
+            props.setInputPokemonTeam(v);
+            setFetchPokemonTeamData(true);
+        }
+     };
+
+     useEffect(() => {
+        if(fetchPokemonTeamData){
+            props.setPokemonTeamData([]);
+            for(var p of props.inputPokemonTeam){
+                if(p.includes("♀")) p = "nidoran-f";
+                if(p.includes("♂")) p = "nidoran-m";
+                if(p === "Mr. Mime") p = "mr-mime";
+                if(p === "Mr. Rime") p = "mr-rime";
+                fetch("https://pokeapi.co/api/v2/pokemon/" + p.toLowerCase())
+                .then((res) => {
+                    return res.json();
+                })
+                .then((resJson) => {
+                    props.setPokemonTeamData(old => [...old, resJson]);
+                });
+            }
+            setFetchPokemonTeamData(false);
+        }
+     }, [fetchPokemonTeamData]);
 
     return(
         <div style={{display: 'flex', justifyContent: 'center'}}> 
             <Autocomplete
                 multiple
-                autoSelect
                 autoHighlight
                 id="pokemonTeam"
-                value={pokemonTeam}
+                value={props.inputPokemonTeam}
                 options={pokemon.all()}
                 sx={{width: '50%', m: 5}}
                 filterOptions={createFilterOptions({matchFrom: 'start'})}
-                onChange = {(e, v) => {if(pokemonTeam.length < 6) setPokemonTeam(v)}}
+                onChange = {(e, v) => {handleInputChange(v)}}
                 getOptionLabel={(option) => option}
                 renderInput={(params) => <TextField {...params} label="Pokemon Team" />}
             />
