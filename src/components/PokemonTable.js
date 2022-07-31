02ea1ec555591f9fc43b51e9d4988be2dc7ed907
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2022-07-30 20:24:00 -0500

    changes to table

--- src/components/PokemonTable.js
+++ src/components/PokemonTable.js
@@ -1,35 +1,50 @@
 import { Table, TableBody, TableRow, TableCell, TableHead } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+const tableCellDefaultSyle = {py: 0.5, width: "12.5%", borderRadius: 1, border: 'none', color: "whitesmoke", textAlign: 'center'};
 
 export default function PokemonTable(props){
     
     var pokemonTypes = require('../json/pokemonTypes.json');
 
-    const [tableCells, setTableCells] = useState(new Array(18).fill(new Array(7).fill('')));
-
-    const updateTableCell = (row, col ,e) => {
-        let newTableCells = [];
-        tableCells.forEach((r) => {
-            let newRow = [];
-            r.forEach((c) => {
-                newRow.push(c);
-            });
-            newTableCells.push(newRow);
-        });
-        newTableCells[row][col] = e;
-        setTableCells(newTableCells);
-    };
+    //const [tableCells, setTableCells] = useState(new Array(18).fill(new Array(7).fill('')));
+    const tableCells = useRef(Array.from(Array(18), () => new Array(7)));
+    // const updateTableCell = (row, col ,e) => {
+    //     let newTableCells = [];
+    //     tableCells.forEach((r) => {
+    //         let newRow = [];
+    //         r.forEach((c) => {
+    //             newRow.push(c);
+    //         });
+    //         newTableCells.push(newRow);
+    //     });
+    //     newTableCells[row][col] = e;
+    //     setTableCells(newTableCells);
+    // };
+    const clearTableCells = () => {
+        for(let i=0;i<18;i++){
+            for(let j=0;j<7;j++){
+                tableCells.current[i][j].innerHTML = '';
+                tableCells.current[i][j].style = tableCellDefaultSyle;
+            }
+        }
+    }
 
     useEffect(() => {
+        //setTableCells(new Array(18).fill(new Array(7).fill('')));
+        //clearTableCells();
+        
         props.pokemonTeamData.forEach((p, pIndex) => {
             p.types.forEach((t) => {
                 fetch('https://pokeapi.co/api/v2/type/'+t.type.name)
                 .then(res => res.json())
                 .then(type1 => {
-                    console.log(type1.damage_relations.no_damage_from);
                     pokemonTypes.forEach((type2, tIndex) => {
                         if(type1.damage_relations.no_damage_from.filter((ty) => ty.name === type2.name.toLowerCase()).length > 0){
-                            updateTableCell(tIndex,pIndex,'X');
+                            if(tableCells.current[tIndex][pIndex].innerHTML !== 'X'){
+                                tableCells.current[tIndex][pIndex].appendChild(document.createTextNode("X"));
+                                tableCells.current[tIndex][pIndex].style.backgroundColor = "gray";
+                            }
                         }
                     });
                 });
@@ -39,10 +54,10 @@ export default function PokemonTable(props){
 
     return(
         <div style={{display: 'flex', justifyContent: 'center'}}> 
-            <Table sx={{width: "75%", border: 'whitesmoke'}}>
+            <Table sx={{width: "75%", border: '1px solid rgb(224,224,224)', borderRadius: 1, borderCollapse: 'separate'}}>
                 <TableHead>
                     <TableRow>
-                        <TableCell sx={{py: 0.5, width:"12.5%"}}></TableCell>
+                        <TableCell sx={{py: 0.5, width:"12.5%", borderRight: '1px solid rgb(224,224,224)'}}></TableCell>
                         {props.pokemonTeamData.map((pokemon, i) => {
                             return(
                                 <TableCell sx={{p: 0, width:"12.5%"}} key={i}>
@@ -60,11 +75,11 @@ export default function PokemonTable(props){
                     {pokemonTypes.map((t, i) => {
                         return(
                         <TableRow sx={{backgroundColor: (i%2 ? "white" : "whitesmoke")}} key={i}>
-                            <TableCell sx={{py: 0.5, color: "whitesmoke",backgroundColor: t.color, borderRadius: 3}}>
+                            <TableCell sx={{py: 0.5, color: "whitesmoke", backgroundColor: t.color, borderRadius: 1}}>
                                 {t.name}
                             </TableCell>
                             {Array.from({length: 7}, (v, j) => {
-                                return <TableCell sx={{py: 0.5, width:"12.5%"}} key={j}>{tableCells[i][j]}</TableCell>
+                                return <TableCell sx={tableCellDefaultSyle} key={j} ref={el => tableCells.current[i][j] = el}></TableCell>
                             })}
                         </TableRow>
                         )
